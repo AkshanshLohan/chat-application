85d9007ecbb455a481da3e9bd7649efcfc8505d6
Akshansh Lohan <akshanshlohan@gmail.com>
2025-08-24 17:10:59 +0530

    added socket.io for displaying instant messages and completed my backendd

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/messageController";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController";
  const MessageRouter=express.Router();
 
  MessageRouter.get("/users",protectRoute,getUsersForSidebar)
  MessageRouter.get("/:id",protectRoute,getMessages)
  MessageRouter.put("mark/:id",protectRoute,markMessageAsSeen)
+ MessageRouter.post("/send/:id",protectRoute, sendMessage)
 
  export default MessageRouter;
